267f37a7a6b672c41c5ef5cf29f5855756403b95
mufidbrown <ilhammufid67@gmail.com>
2024-06-16 01:09:00 +0700

    first commit github

---
+++ src/main/java/com/koperasi/simpan_pinjam_koperasi/SimpanPinjamKoperasiApplication.java
@@ -0,0 +1,13 @@
+package com.koperasi.simpan_pinjam_koperasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpanPinjamKoperasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpanPinjamKoperasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koperasi/simpan_pinjam_koperasi/SimpanPinjamKoperasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koperasi.simpan_pinjam_koperasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpanPinjamKoperasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
